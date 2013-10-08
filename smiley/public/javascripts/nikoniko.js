62f46ceb66865544d1a525d815a340da9facd325
Tom Westmacott <tom.westmacott@timgroup.com>
2013-10-08 15:10:02 +0100

    Shaf/TomW: list names in table

--- smiley/public/javascripts/nikoniko.js
+++ smiley/public/javascripts/nikoniko.js
@@ -1,5 +1,13 @@
-$(
+$(function() {
 
   $.get( "/smilies", function(data) {
-    alert("We got: " + JSON.stringify(data));
-  }));
+    $.each(data, function(name, smilies) {
+      var row = $("<tr/>"), label = $("<td>" + name + "</td>");
+      row.append(label);
+      $(".nikoniko-individuals").append(row);
+    });
+
+//    alert("We got: " + JSON.stringify(data));
+  });
+
+});
