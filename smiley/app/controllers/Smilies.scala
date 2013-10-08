aac425f2fe78446002883ba97ee0cab6d02784fb
Tom Westmacott <tom.westmacott@timgroup.com>
2013-10-08 12:57:44 +0100

    Shaf/TomW: added stub data set

--- smiley/app/controllers/Smilies.scala
+++ smiley/app/controllers/Smilies.scala
@@ -9,7 +9,31 @@ object Smilies extends Controller {
 
   def lastTwoWeeks = Action {
 
-    val data = Map("shaf" -> Map("2013-10-08" -> "happy"))
+    val data = Map("Shaf" -> Map(
+        "2013-09-30" -> "unhappy",
+        "2013-10-01" -> "happy",
+        "2013-10-02" -> "meh",
+        "2013-10-03" -> "meh",
+        "2013-10-04" -> "happy",
+        "2013-10-07" -> "happy",
+        "2013-10-08" -> "happy"
+        ),
+        "Tom W" -> Map(
+        "2013-10-01" -> "happy",
+        "2013-10-02" -> "meh",
+        "2013-10-03" -> "meh",
+        "2013-10-04" -> "unhappy",
+        "2013-10-07" -> "meh"
+        ),
+        "Sergiusz" -> Map(
+        "2013-09-30" -> "happy",
+        "2013-10-01" -> "happy",
+        "2013-10-02" -> "meh",
+        "2013-10-03" -> "meh",
+        "2013-10-04" -> "happy",
+        "2013-10-08" -> "meh"
+        )
+        )
 
     Ok(toJson(data))
   }
