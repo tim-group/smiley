23d3b93adc4e2ad5afb684c62ebea912deb70eba
Tom Westmacott <tom.westmacott@timgroup.com>
2013-10-08 16:16:28 +0100

    Shaf/TomW: display sentiment for each day

--- smiley/public/javascripts/nikoniko.js
+++ smiley/public/javascripts/nikoniko.js
@@ -7,11 +7,40 @@ $(function() {
 
   var fromDate = lastMonday();
 
+  var ratingSymbol = {
+    "happy": "H",
+    "neutral": "N",
+    "sad": "S"
+  };
+
+
+  function renderWeek(fromDate, smilies, row) {
+    var startDate = new XDate(fromDate);
+     for(i = 0; i < 5; i++) {
+        var dateKey = startDate.toString("yyyy-MM-dd");
+        var rating = smilies[dateKey];
+        var td = $("<td></td>");
+        if (rating) {
+          td.text(ratingSymbol[rating]);
+        }
+        row.append(td);
+        startDate.addDays(1);
+      }
+  }
+
+
   $.get( "/smilies/" + fromDate.toString("yyyy-MM-dd"), function(data) {
     $.each(data, function(name, smilies) {
-      var row = $("<tr/>"), label = $("<td>" + name + "</td>");
+      var row = $("<tr/>"), label = $("<td>" + name + "</td>"), startDate = new XDate(fromDate);
       row.append(label);
+
+      renderWeek(startDate, smilies, row);
+      startDate.addDays(7);
+      row.append("<td/>");
+      renderWeek(startDate, smilies, row);
+
       $(".nikoniko-individuals").append(row);
+
     });
 
 //    alert("We got: " + JSON.stringify(data));
