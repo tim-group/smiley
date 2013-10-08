615445670509700b7c70c615472bc5e9d13c1226
Sergiusz <sergiusz.bleja@timgroup.com>
2013-10-08 12:24:42 +0100

    Sergiusz: Adding personal greeting in the email

--- client/email-reminder.sh
+++ client/email-reminder.sh
@@ -2,5 +2,7 @@
 
 for rec in `cat recipients.txt`
 do 
-    mail -a "Content-type: text/html;" -s "Happy?"  $rec < email.html
+    cat email.html | sed -e "s/\${email}/$rec/" > email.to.send
+    mail -a "Content-type: text/html;" -s "Happy?"  $rec < email.to.send
+    unlink email.to.send
 done
