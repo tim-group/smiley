5c8811de89b711eece5d83d90851b52b6dfec1d2
Shafeen <shafeen.tejani@timgroup.com>
2013-10-11 14:38:41 +0100

    Shaf: Add sentiment thermometer.

--- smiley/public/javascripts/nikoniko.js
+++ smiley/public/javascripts/nikoniko.js
@@ -5,8 +5,6 @@ $(function() {
     return new XDate().setWeek(lastWeek.getWeek(), lastWeek.getFullYear());
   }
 
-  var fromDate = lastMonday();
-
   var ratingSymbol = {
     "happy": "<img src='/assets/images/email-smile.png'/>",
     "neutral": "<img src='/assets/images/email-neutral.png'/>",
@@ -23,38 +21,90 @@ $(function() {
     return firstName.slice(0, 1).toUpperCase() + firstName.substr(1) + " " + lastName.slice(0, 1).toUpperCase();
   }
 
-  function renderWeek(fromDate, smilies, row) {
-    var startDate = new XDate(fromDate);
-     for(i = 0; i < 5; i++) {
-        var dateKey = startDate.toString("yyyy-MM-dd");
-        var rating = smilies[dateKey];
-        var td = $("<td class='sentiment'></td>");
-        if (rating && ratingSymbol[rating]) {
-          td.html(ratingSymbol[rating]);
-        }
-        row.append(td);
-        startDate.addDays(1);
-      }
+  function renderWeek(week, smilies, row) {
+      _.each(week, function(day) {
+          var rating = smilies[day];
+          var td = $("<td class='sentiment'></td>");
+          if (rating && ratingSymbol[rating]) {
+              td.html(ratingSymbol[rating]);
+          }
+          row.append(td);
+      });
   }
 
-  function renderOnePersonsSentiment(data) {
+  function renderOnePersonsSentiment(data, weeks) {
     $(".nikoniko-individuals").empty();
     $.each(data, function(name, smilies) {
-      var row = $("<tr/>"), label = $("<td class='name'>" + deriveReadableName(name) + "</td>"), startDate = new XDate(fromDate);
+      var row = $("<tr/>"), label = $("<td class='name'>" + deriveReadableName(name) + "</td>");
       row.append(label);
 
-      renderWeek(startDate, smilies, row);
-      startDate.addDays(7);
-      row.append("<td class='weekend'/>");
-      renderWeek(startDate, smilies, row);
+        _.each(weeks, function(week) {
+            renderWeek(week, smilies, row);
+            row.append("<td class='weekend'/>");
+        });
+
 
       $(".nikoniko-individuals").append(row);
 
     });
+
+  }
+
+
+  function renderTotalSentiment(data, dates) {
+      var counts = totalSentiment(data, dates);
+      var total = counts["happy"] + counts["sad"] + counts["neutral"];
+      var happy = percentOf(counts["happy"], total);
+      var sad = percentOf(counts["sad"], total);
+
+      $(".happy-bar").height(parseInt(happy) + "%");
+      $(".sad-bar").height(parseInt(sad) + "%")
+  }
+
+  function totalSentiment(data, datesToCalculateSentimentFor) {
+      var counts = { "happy" : 0, "sad" : 0, "neutral" : 0};
+      $.each(data, function(name, smilies) {
+          $.each(smilies, function(date, sentiment) {
+              if (_.contains(datesToCalculateSentimentFor, date)) {
+                  counts[sentiment] += 1;
+              }
+          });
+      });
+      return counts;
+  }
+
+  function daysInWeeksFrom(fromDate, numWeeks) {
+      var startDate = new XDate(fromDate);
+      var dates = [];
+      for(var i = 0; i < numWeeks; i++) {
+          dates.push(daysInWeek(startDate));
+          startDate.addDays(7);
+      }
+      return dates;
+  }
+
+  function daysInWeek(fromDate) {
+      var startDate = new XDate(fromDate);
+      var dates = [];
+      for(var i = 0; i < 5; i++) {
+          dates.push(startDate.toString("yyyy-MM-dd"));
+          startDate.addDays(1);
+      }
+      return dates;
+  }
+
+
+  function percentOf(num, total) {
+    return (num / total) * 100.0;
   }
 
   function populateSentiments() {
-    $.get( "/smilies/" + fromDate.toString("yyyy-MM-dd"), renderOnePersonsSentiment);
+    var daysInWeeks = daysInWeeksFrom(lastMonday(), 2);
+
+    $.get( "/smilies/" + daysInWeeks[0][0], function(data) {
+        renderOnePersonsSentiment(data, daysInWeeks);
+        renderTotalSentiment(data, _.flatten(daysInWeeks));
+    });
   }
 
   populateSentiments();
